0266e8bf4e9c3fdbec3393b963ffc0d002783974
alleappio <allevanny@gmail.com>
2024-11-07 12:10:03 +0100

    change nvim lsp

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return{
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "cmake", "pyright" }
+        ensure_installed = { "lua_ls", "clangd", "cmake", "pylsp" }
       })
     end
   },
