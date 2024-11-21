c2e6d1a59ff4e8c24fcd98c1acc279700dbf3068
alleappio <allevanny@gmail.com>
2024-11-21 16:20:18 +0100

    first edits

---
+++ nvim/.config/nvim/lua/plugins/everforest.lua
@@ -0,0 +1,10 @@
+return {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_enable_italic = true
+      vim.g.everforest_background = 'soft'
+      vim.cmd.colorscheme('everforest')
+    end
+}
