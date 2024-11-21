22fb12571b4038babc3a94c69aa18c79a826da14
alleappio <allevanny@gmail.com>
2024-11-21 17:18:18 +0100

    edit nvim color

--- nvim/.config/nvim/lua/plugins/everforest.lua
+++ nvim/.config/nvim/lua/plugins/everforest.lua
@@ -4,7 +4,7 @@ return {
     priority = 1000,
     config = function()
       vim.g.everforest_enable_italic = true
-      vim.g.everforest_background = 'soft'
+      vim.g.everforest_background = 'hard'
       vim.cmd.colorscheme('everforest')
     end
 }
