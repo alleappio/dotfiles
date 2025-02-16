7abcb4b2a308912d00ef4294b682cac27101c0b2
alleappio <allevanny@gmail.com>
2025-02-16 15:09:18 +0100

    edit polybar

--- i3/.config/i3/polybar.sh
+++ i3/.config/i3/polybar.sh
@@ -15,6 +15,7 @@ polybar-msg cmd quit
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload bar1 &
+    MONITOR=$m polybar --reload bar2 &
   done
 else
   polybar --reload example &
