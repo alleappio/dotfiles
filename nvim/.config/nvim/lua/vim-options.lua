e64da3ce810512f29ab49605e5392be7bee4f7d6
alleappio <allevanny@gmail.com>
2025-04-27 18:48:43 +0200

    edit nvim

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,7 +1,16 @@
+-- VIM OPTIONS
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.cmd("set clipboard=unnamedplus")
+
+-- REMAPPINGS
 vim.g.mapleader=" "
-vim.api.nvim_set_option("clipboard","unnamed") 
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>o", "o<ESC>k")
+vim.keymap.set("n", "<leader>O", "O<ESC>j")
+
