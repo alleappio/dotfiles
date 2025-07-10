ac8c4d30d7fb0774478478dcbbec4fe0c742784a
alleappio <allevanny@gmail.com>
2025-07-11 00:12:05 +0200

    add vim options
    Signed-off-by: alleappio <allevanny@gmail.com>

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -3,9 +3,14 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set relativenumber")
 vim.cmd("set clipboard=unnamedplus")
 vim.cmd("set scrolloff=10")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+vim.opt.autoindent = true
+vim.cmd("colorscheme onedark")
+vim.opt.autoread = true
 
 -- REMAPPINGS
 vim.keymap.set("n", "<leader>y", "\"+y")
@@ -13,4 +18,3 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>o", "o<ESC>k")
 vim.keymap.set("n", "<leader>O", "O<ESC>j")
-vim.cmd("colorscheme onedark")
