91d6e2c9f877bb4200718c7f9f30331e789cff8c
alleappio <allevanny@gmail.com>
2025-09-10 02:07:41 +0200

    update install script
    Signed-off-by: alleappio <allevanny@gmail.com>

--- install.sh
+++ install.sh
@@ -1,13 +1,63 @@
 #!/bin/bash
-stow desktop_apps
+
+echo "enabling solopasha/hyprland copr"
+sudo dnf copr enable solopasha/hyprland
+
+echo "installing required packages"
+sudo dnf install -y zsh \
+  fastfetch \
+  neovim \
+  tmux \
+  kitty \
+  bemenu \
+  waybar \
+  swaync \
+  swaybg \
+  hyprland \
+  swaync \
+  hypridle \
+  hyprlock \
+  slurp \
+  grim \
+  xclip \
+  pavucontrol \
+  curl
+
+echo "installing oh-my-zsh, please follow the instructions provided by its installer"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "copying zsh files"
+cp zsh/.zshrc ~
+cp -r zsh/.oh-my-zsh/custom/plugins/* ~/.oh-my-zsh/custom/plugins
+cp -r zsh/.oh-my-zsh/custom/themes/* ~/.oh-my-zsh/custom/themes
+
+echo "stowing fastfetch"
 stow fastfetch
-stow i3
-stow kitty
-stow hypr
+
+echo "stowing nvim"
 stow nvim
-stow polybar
-stow rofi
+
+echo "stowing tmux"
 stow tmux
+
+echo "stowing kitty"
+stow kitty
+
+echo "stowing bemenu"
+stow bemenu
+
+echo "stowing waybar"
 stow waybar
-stow zsh
+
+echo "stowing swaync"
+stow swaync
+
+echo "copying wallpaper"
 cp wallpaper/.config/background ~/.config/background
+
+echo "stowing hypr"
+stow hypr
+
+echo "make flatpaks visible to bemenu"
+chmod +x extract_flatpak_bins.sh
+./extract_flatpak_bins.sh
