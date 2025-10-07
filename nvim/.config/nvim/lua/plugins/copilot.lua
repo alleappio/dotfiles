69d310c4f9c1c12479be9ba9415caf76c8b1a3dd
alleappio <allevanny@gmail.com>
2025-10-07 23:04:32 +0200

    add copilot plugin
    Signed-off-by: alleappio <allevanny@gmail.com>

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+-- ~/.config/nvim/lua/plugins/copilot.lua
+return {
+  {
+    "github/copilot.vim",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      -- Recommended config
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
+      
+      -- Map <C-J> to accept Copilot suggestion
+      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end,
+  },
+}
