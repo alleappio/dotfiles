ffa8d97cb05e02c03a66b3180d0bd27e03ea5a4f
alleappio <allevanny@gmail.com>
2025-10-07 20:57:28 +0200

    edit tab settings
    Signed-off-by: alleappio <allevanny@gmail.com>

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,10 +1,9 @@
 -- VIM OPTIONS
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard=unnamedplus")
 vim.cmd("set scrolloff=10")
+vim.cmd("set shiftwidth=4 smarttab")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=8 softtabstop=0")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
