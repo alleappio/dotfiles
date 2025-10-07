703c0e503cab961ba06d34d425bb39843f954eef
alleappio <allevanny@gmail.com>
2025-10-07 21:50:52 +0200

    add custom greeter
    Signed-off-by: alleappio <allevanny@gmail.com>

---
+++ nvim/.config/nvim/lua/custom-greeter.lua
@@ -0,0 +1,57 @@
+local function center_lines(lines)
+    local width = vim.api.nvim_get_option_value("columns", {})
+    local centered = {}
+    for _, line in ipairs(lines) do
+        local padding = math.floor((width - #line) / 2)
+        if padding > 0 then
+            table.insert(centered, string.rep(" ", padding) .. line)
+        else
+            table.insert(centered, line)
+        end
+    end
+    return centered
+end
+
+local function show_greeting()
+  if vim.fn.argc() == 0 then
+    vim.cmd('enew')  -- open an empty buffer
+    local buf = vim.api.nvim_get_current_buf()
+
+    vim.bo[buf].buftype = 'nofile'
+    vim.bo[buf].bufhidden = 'wipe'
+    vim.bo[buf].swapfile = false
+    vim.bo[buf].modifiable = true
+
+    vim.api.nvim_set_option_value('number', false, { scope = 'local', win = 0 })
+    vim.api.nvim_set_option_value('relativenumber', false, { scope = 'local', win = 0 })
+
+    local header = {
+        "NeoVim",
+        "",
+    }
+
+    local lines = {
+        "",
+        "",
+        "Hello There!",
+    }
+
+    local final_lines = {}
+    vim.list_extend(final_lines, header)
+    vim.list_extend(final_lines, lines)
+    final_lines = center_lines(final_lines)
+
+    local height = vim.api.nvim_get_option_value("lines", {})
+    local top_padding = math.floor((height/2)-(#final_lines/2)-1)
+    for _ = 1, top_padding do
+        table.insert(final_lines, 1, "")
+    end
+
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, final_lines)
+    vim.bo[buf].modifiable = false
+  end
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = show_greeting,
+})
