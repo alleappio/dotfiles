84a9f7aab8d9c63157eb971d8c1d9f9beced855d
alleappio <allevanny@gmail.com>
2025-10-07 23:37:15 +0200

    edit scrolloff
    Signed-off-by: alleappio <allevanny@gmail.com>

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,6 +1,6 @@
 -- VIM OPTIONS
 vim.cmd("set clipboard=unnamedplus")
-vim.cmd("set scrolloff=10")
+vim.cmd("set scrolloff=15")
 vim.cmd("set shiftwidth=4 smarttab")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=8 softtabstop=0")
