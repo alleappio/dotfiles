7369cefeaa0dbe2362088dd226dd593ee5e6c118
alleappio <allevanny@gmail.com>
2025-10-07 23:34:57 +0200

    edit copilot accept keybind
    Signed-off-by: alleappio <allevanny@gmail.com>

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -5,13 +5,13 @@ return {
     cmd = "Copilot",
     event = "InsertEnter",
     config = function()
-      -- Recommended config
-      vim.g.copilot_no_tab_map = true
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_tab_fallback = ""
-      
-      -- Map <C-J> to accept Copilot suggestion
-      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    -- Recommended config
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_assume_mapped = true
+    vim.g.copilot_tab_fallback = ""
+
+    -- Map <C-J> to accept Copilot suggestion
+    vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
     end,
   },
 }
