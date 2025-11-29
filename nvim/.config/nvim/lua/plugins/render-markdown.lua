d743a959907db3b67fcd662a28ca272f2934f35b
aappio <alle.appio@gmail.com>
2025-11-29 17:54:52 +0100

    add render markdown
    Signed-off-by: aappio <alle.appio@gmail.com>

---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,29 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    config = function()
+        require('render-markdown').setup({
+            completions = { lsp = { enabled = true } },
+            preset = "obsidian",
+            heading = {
+                backgrounds = {
+                    nil,
+                    nil,
+                    nil,
+                    nil,
+                    nil,
+                    nil,
+                },
+            },
+            latex = {
+                enabled = true,
+                render_modes = false,
+                converter = { 'utftex', 'latex2text' },
+                highlight = 'RenderMarkdownMath',
+                position = 'center',
+                top_pad = 0,
+                bottom_pad = 0,
+            },
+        })
+    end,
+}
