885096c983ab74440b9d636bdf9c3db4180fbd69
aappio <alle.appio@gmail.com>
2025-12-27 11:42:52 +0100

    add indent blank line in nvim
    Signed-off-by: aappio <alle.appio@gmail.com>

---
+++ nvim/.config/nvim/lua/plugins/ibl.lua
@@ -0,0 +1,17 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+    config = function()
+        local configs = require("ibl").setup({
+            scope = {
+                enabled=false,
+            },
+            indent = {
+                char = "┆",
+            }
+        })
+    end,
+}
