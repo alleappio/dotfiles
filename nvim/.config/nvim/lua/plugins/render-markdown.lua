2c9dc1632f49499c7ae6efd7a79779d5525553a4
aappio <alle.appio@gmail.com>
2026-01-10 01:25:01 +0100

    nvim: add markdown preview
    Signed-off-by: aappio <alle.appio@gmail.com>

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -17,13 +17,7 @@ return {
                 },
             },
             latex = {
-                enabled = true,
-                render_modes = false,
-                converter = { 'utftex', 'latex2text' },
-                highlight = 'RenderMarkdownMath',
-                position = 'center',
-                top_pad = 0,
-                bottom_pad = 0,
+                enabled = false,
             },
         })
     end,
