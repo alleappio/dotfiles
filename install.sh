b8dd6f260e47925f91b7a373dfeda512c24c1429
aappio <alle.appio@gmail.com>
2026-02-09 13:40:30 +0100

    install: edit install for pacman
    Signed-off-by: aappio <alle.appio@gmail.com>

--- install.sh
+++ install.sh
@@ -32,40 +32,40 @@ if command -v dnf &>/dev/null; then
         fuzzel
 elif command -v pacman &>/dev/null; then
     echo "pacman command detected, running install trough pacman"
-    sudo pacman -S stow  
-    sudo pacman -S fastfetch  
-    sudo pacman -S neovim  
-    sudo pacman -S tmux  
-    sudo pacman -S kitty  
-    sudo pacman -S bemenu  
-    sudo pacman -S waybar  
-    sudo pacman -S swaync  
-    sudo pacman -S swaybg  
-    sudo pacman -S hyprland  
-    sudo pacman -S hypridle  
-    sudo pacman -S hyprlock  
-    sudo pacman -S hyprpolkitagent  
-    sudo pacman -S hyprland-guiutils  
-    sudo pacman -S hyprlauncher  
-    sudo pacman -S hyprshot  
-    sudo pacman -S swaync  
-    sudo pacman -S pavucontrol  
-    sudo pacman -S curl  
-    sudo pacman -S NetworkManager-tui  
-    sudo pacman -S gnome-extensions  
-    sudo pacman -S gnome-extensions-app  
-    sudo pacman -S utftex  
-    sudo pacman -S luarocks
-    sudo pacman -S fuzzel
-    sudo pacman -S tree-sitter-cli
-    sudo pacman -S nodejs
-    sudo pacman -S npm
+    sudo pacman -S stow \
+     fastfetch \
+     neovim \
+     tmux \
+     kitty \
+     bemenu \
+     waybar \
+     swaync \
+     swaybg \
+     hyprland \
+     hypridle \
+     hyprlock \
+     hyprpolkitagent \
+     hyprland-guiutils \
+     hyprlauncher \
+     hyprshot \
+     swaync \
+     pavucontrol \
+     curl \
+     luarock\
+     fuzze\
+     tree-sitter-cl\
+     nodej\
+     npm
 else
     echo "nor pacman and dnf detected, make sure that one of those exist and rerun this command"
     exit
 fi
 
 echo "stowing bash"
+rm ~/.bashrc
+if [ -d ~/.bashrc.d ]; then
+    rm -r .bashrc.d
+fi
 stow bash
 
 echo "stowing fastfetch"
@@ -83,6 +83,9 @@ stow kitty
 echo "stowing bemenu"
 stow bemenu
 
+echo "stowing fuzzel"
+stow fuzzel
+
 echo "stowing waybar"
 stow waybar
 
@@ -94,10 +97,6 @@ stow wallpaper
 
 echo "stowing hyprland"
 stow hypr
-
-echo "make flatpaks visible to bemenu"
-chmod +x extract_flatpak_bins.sh
-./extract_flatpak_bins.sh
 
 echo "Select a theme"
 chmod +x change_theme.sh
