d2485fab0bbb541c290a6d8790386592067ba9e2
aappio <alle.appio@gmail.com>
2026-02-09 17:33:09 +0100

    nvim:update treesitter config
    Signed-off-by: aappio <alle.appio@gmail.com>

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,12 @@ return{
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config=function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
+    require'nvim-treesitter'.setup {
       ensure_installed = { "cmake", "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "cpp", "python", "rust", "latex" }, 
       auto_install = true,
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
-    })
+    }
   end
 }
