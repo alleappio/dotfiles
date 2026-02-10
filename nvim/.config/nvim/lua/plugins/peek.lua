91d3ea6ffa87407b54b55be6afbd315bde6eae80
aappio <alle.appio@gmail.com>
2026-02-10 18:53:30 +0100

    nvim: remove default application for peek
    Signed-off-by: aappio <alle.appio@gmail.com>

--- nvim/.config/nvim/lua/plugins/peek.lua
+++ nvim/.config/nvim/lua/plugins/peek.lua
@@ -3,9 +3,9 @@ return {
     event = { "VeryLazy" },
     build = "deno task --quiet build:fast",
     config = function()
-        require("peek").setup({
-            app = 'firefox',
-        })
+--        require("peek").setup({
+--            app = 'firefox',
+--        })
         vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
         vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
     end,
