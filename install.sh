7848ad7394fb5c6361aa5f3469da11963ca85e47
aappio <alle.appio@gmail.com>
2026-02-15 20:51:54 +0100

    cleanup
    Signed-off-by: aappio <alle.appio@gmail.com>

--- install.sh
+++ install.sh
@@ -3,40 +3,7 @@
 git submodule update --init
 
 echo "installing required packages"
-if command -v dnf &>/dev/null; then
-    echo "dnf command detected, running install trhough dnf"
-    sudo dnf install -y stow \
-        fastfetch \
-        neovim \
-        tmux \
-        kitty \
-        bemenu \
-        waybar \
-        swaync \
-        swaybg \
-        hyprland \
-        hypridle \
-        hyprlock \
-        hyprpolkitagent \
-        hyprland-guiutils \
-        hyprlauncher \
-        hyprshot \
-        swaync \
-        pavucontrol \
-        curl \
-        NetworkManager-tui \
-        gnome-extensions \
-        gnome-extensions-app \
-        utftex \
-        luarocks \
-        fuzzel
-elif command -v pacman &>/dev/null; then
-    echo "pacman command detected, running install trough pacman"
-    sudo pacman -S --needed $(cat arch_pkglist.txt)
-else
-    echo "nor pacman and dnf detected, make sure that one of those exist and rerun this command"
-    exit
-fi
+yay -S --needed $(cat arch_pkglist.txt)
 
 echo "stowing bash"
 rm ~/.bashrc
@@ -57,23 +24,17 @@ stow tmux
 echo "stowing kitty"
 stow kitty
 
-echo "stowing bemenu"
-stow bemenu
-
-echo "stowing fuzzel"
-stow fuzzel
-
 echo "stowing waybar"
 stow waybar
 
 echo "stowing swaync"
 stow swaync
 
-echo "copying wallpaper"
-stow wallpaper
-
 echo "stowing hyprland"
 stow hypr
+
+echo "stowing walker"
+stow walker
 
 echo "Select a theme"
 chmod +x change_theme.sh
