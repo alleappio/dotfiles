cfdc4a264c10f45dbe77840dc2dc62eef6be2d81
aappio <alle.appio@gmail.com>
2026-02-23 15:23:04 +0100

    create wallpaper picker
    Signed-off-by: aappio <alle.appio@gmail.com>

--- rofi/.config/rofi/scripts/wallpaper_picker.sh
+++ rofi/.config/rofi/scripts/wallpaper_picker.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-export PREVIEW=true
-rofi -theme ~/.config/rofi/wallpaper_picker/theme.rasi \
-        -show filebrowser \
-        -filebrowser-directory ~/dotfiles/wallpaper
+change_wallpaper(){
+    ln -fs $1 ~/.config/background;
+    pkill hyprpaper; hyprctl dispatch exec "hyprpaper";
+}
+
+rofi_command(){
+    export PREVIEW=true
+    rofi -theme ~/.config/rofi/wallpaper_picker/theme.rasi \
+            -show filebrowser \
+            -filebrowser-directory ~/dotfiles/wallpaper \
+            -filebrowser-command echo
+}
+
+new_wallpaper=$(rofi_command);
+change_wallpaper $new_wallpaper;
