13f032a0b64a1cb7e08f1e39c67ba7052cdd2f86
aappio <alle.appio@gmail.com>
2026-02-28 11:35:12 +0100

    nvim: remove unused telescope extension
    Signed-off-by: aappio <alle.appio@gmail.com>

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,17 +5,4 @@ return{
     config = function()
     end
   },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown{}
-          }
-        }
-      })
-      require("telescope").load_extension("ui-select")
-    end
-  },
 }
