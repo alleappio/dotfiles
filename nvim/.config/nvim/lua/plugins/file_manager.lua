86d889ae152d5d1f6e4f623311568f215910019d
aappio <alle.appio@gmail.com>
2026-02-28 11:34:38 +0100

    nvim: from fyler to oil
    Signed-off-by: aappio <alle.appio@gmail.com>

---
+++ nvim/.config/nvim/lua/plugins/file_manager.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {},
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("oil").setup({
+                default_file_explorer = true,
+                skip_confirm_for_simple_edits = true,
+                columns = {
+                    "icon",
+                    "permissions",
+                },
+                float = {
+                    padding = 2,
+                    max_width = 0.5,
+                    max_height = 0.5,
+                    border = "rounded",
+                },
+                view_options = {
+                    show_hidden = true,
+                },
+            })
+        end
+    },
+    {
+      "benomahony/oil-git.nvim",
+      dependencies = { "stevearc/oil.nvim" },
+      -- No opts or config needed! Works automatically
+    },
+}
