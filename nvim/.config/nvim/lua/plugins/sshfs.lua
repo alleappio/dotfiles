32ce10505450cc824ac5d8b07ad028accc52bd11
aappio <alle.appio@gmail.com>
2026-03-11 22:49:14 +0100

    nvim: update sshfs
    Signed-off-by: aappio <alle.appio@gmail.com>

--- nvim/.config/nvim/lua/plugins/sshfs.lua
+++ nvim/.config/nvim/lua/plugins/sshfs.lua
@@ -2,7 +2,7 @@ return {
     "uhs-robert/sshfs.nvim",
     opts = {
         ui = {
-            file_picker = {
+            local_picker = {
                 preferred_picker = "oil",  -- one of: "auto", "snacks", "fzf-lua", "mini", "telescope", "oil", "neo-tree", "nvim-tree", "yazi", "lf", "nnn", "ranger", "netrw"
                 fallback_to_netrw = true,   -- fallback to netrw if no picker is available
                 netrw_command = "Explore",  -- netrw command: "Explore", "Lexplore", "Sexplore", "Vexplore", "Texplore"
