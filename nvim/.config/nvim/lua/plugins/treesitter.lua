226a39f215ce27ac4544adf95398ecf889059acf
aappio <alle.appio@gmail.com>
2026-03-16 15:27:47 +0100

    nvim: update treesitter config to the new one
    Signed-off-by: aappio <alle.appio@gmail.com>

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,34 @@
 return{
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  branch='main',
-  lazy=false,
-  config=function()
-    require'nvim-treesitter'.setup {
-      ensure_installed = { "cmake", "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "cpp", "python", "rust", "latex", "markdown", "markdown_inline" },
-      auto_install = true,
-      highlight = { enable = true },
-    }
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = { "markdown", "copilot-chat" },
-      callback = function(ev)
-        vim.treesitter.start(ev.buf)
-      end,
-    })
-    
-  end
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy=false,
+    config=function()
+        require('nvim-treesitter').setup {
+            -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+            install_dir = vim.fn.stdpath('data') .. '/site'
+        }
+        require('nvim-treesitter').install({
+            "cmake",
+            "c",
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+            "javascript",
+            "html",
+            "cpp",
+            "python",
+            "rust",
+            "latex",
+            "markdown",
+            "markdown_inline"
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "markdown", "copilot-chat" },
+            callback = function(ev)
+            vim.treesitter.start(ev.buf)
+            end,
+        })
+    end
 }
