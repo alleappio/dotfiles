2b4bf4586d6e49429dbe31e1882186fcb75b4dd2
aappio <alle.appio@gmail.com>
2026-04-10 00:06:44 +0200

    edit bash theme
    Signed-off-by: aappio <alle.appio@gmail.com>

--- bash/.bashrc.d/style.bash
+++ bash/.bashrc.d/style.bash
@@ -11,20 +11,28 @@ git_status() {
       if [[ -n $(git status --porcelain 2>/dev/null) ]]; then
             dirty="*"
         fi
-        echo "${YELLOW}$branch${RED}$dirty${YELLOW}${RESET}"
+        echo "${YELLOW}($branch${RED}$dirty${YELLOW})${RESET} "
     fi
 }
 
 invirtualenv() {
   if [[ -n "$VIRTUAL_ENV" ]]; then
-    echo "${GREEN}venv${RESET}"
+      echo "${GREEN}(venv)${RESET} "
   fi
+}
+
+bira() {
+  PS1="╭─\u@\h ${BLUE}\w${RESET} $GIT_STATUS $VENV_STATUS${RESET}\n╰─\$ "
+}
+
+custom() {
+  PS1="${BLUE}\w $GIT_STATUS$VENV_STATUS${GREEN}${RESET} "
 }
 
 update_prompt () {
   GIT_STATUS=$(git_status)
   VENV_STATUS=$(invirtualenv)
-  PS1="╭─\u@\h ${BLUE}\w${RESET} $GIT_STATUS $VENV_STATUS${RESET}\n╰─\$ "
+  custom
 }
 
 PROMPT_COMMAND=update_prompt
