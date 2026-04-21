87e6dc8f54b335772b58702031a57f7720b2de96
aappio <alle.appio@gmail.com>
2026-04-21 21:05:56 +0200

    change plasma theme
    Signed-off-by: aappio <alle.appio@gmail.com>

--- plasma/plasma_colors.py
+++ plasma/plasma_colors.py
@@ -12,50 +12,61 @@ def hex_to_rgb(hex: str) -> str:
     r, g, b = int(hex[0:2], 16), int(hex[2:4], 16), int(hex[4:6], 16)
     return f"{r},{g},{b}"
 
-if len(sys.argv) != 2:
-    help()
+def fill_template(template_file, colors, output_file):
+    with open(template_file, "r") as f:
+        template = f.read()
 
-colorscheme_name = sys.argv[1]
-colorscheme_file = f"{os.environ["HOME"]}/dotfiles/themes/{colorscheme_name}/plasma/colors.toml"
-if not os.path.isfile(colorscheme_file):
-    print("colorscheme {colorscheme} not found")
-    help()
+    output = template.format_map(colors)
 
-with open(colorscheme_file, "rb") as f:
-    tot = tomllib.load(f)
-    colorscheme = {}
-    desc = {}
-    try:
-        colorscheme = tot["colors"]
-        desc = tot["description"]
-        found = True
-    except KeyError as e:
-        print(e)
+    with open(output_file, "w") as f:
+        f.write(output)
+
+def gen_plasma_colors(colors):
+    plasma_template_file = f"{os.environ["HOME"]}/dotfiles/plasma/colors.colors.template"
+    plasma_output_file = f"{os.environ["HOME"]}/.local/share/color-schemes/{colors["scheme_id"]}.colors"
+
+    fill_template(plasma_template_file, colors, plasma_output_file)
+
+def main():
+    if len(sys.argv) != 2:
         help()
 
-colorscheme["accent"] = colorscheme[colorscheme["primary"]]
+    colorscheme_name = sys.argv[1]
+    colorscheme_file = f"{os.environ["HOME"]}/dotfiles/themes/{colorscheme_name}/plasma/colors.toml"
+    if not os.path.isfile(colorscheme_file):
+        print("colorscheme {colorscheme} not found")
+        help()
 
-with open(f"{os.environ["HOME"]}/dotfiles/plasma/colors.colors.template") as f:
-    template = f.read()
+    with open(colorscheme_file, "rb") as f:
+        tot = tomllib.load(f)
+        colorscheme = {}
+        desc = {}
+        try:
+            colorscheme = tot["colors"]
+            desc = tot["description"]
+            found = True
+        except KeyError as e:
+            print(e)
+            help()
 
+    colorscheme["accent"] = colorscheme[colorscheme["primary"]]
 
-colors = {
-    "scheme_id":          desc["id"],
-    "scheme_name":        desc["name"],
-    "background":         hex_to_rgb(colorscheme["background"]),
-    "background_alt":     hex_to_rgb(colorscheme["background_alt"]),
-    "foreground":         hex_to_rgb(colorscheme["foreground"]),
-    "foreground_inactive":hex_to_rgb(colorscheme["foreground"]),
-    "red":                hex_to_rgb(colorscheme["red"]),
-    "green":              hex_to_rgb(colorscheme["green"]),
-    "yellow":             hex_to_rgb(colorscheme["yellow"]),
-    "blue":               hex_to_rgb(colorscheme["blue"]),
-    "purple":             hex_to_rgb(colorscheme["magenta"]),
-    "accent":             hex_to_rgb(colorscheme[colorscheme["primary"]]),
-    "hover":              hex_to_rgb(colorscheme["background_alt"]),
-}
+    colors = {
+        "scheme_id":          desc["id"],
+        "scheme_name":        desc["name"],
+        "background":         hex_to_rgb(colorscheme["background"]),
+        "background_alt":     hex_to_rgb(colorscheme["background_alt"]),
+        "foreground":         hex_to_rgb(colorscheme["foreground"]),
+        "foreground_inactive":hex_to_rgb(colorscheme["foreground"]),
+        "red":                hex_to_rgb(colorscheme["red"]),
+        "green":              hex_to_rgb(colorscheme["green"]),
+        "yellow":             hex_to_rgb(colorscheme["yellow"]),
+        "blue":               hex_to_rgb(colorscheme["blue"]),
+        "purple":             hex_to_rgb(colorscheme["magenta"]),
+        "accent":             hex_to_rgb(colorscheme["accent"]),
+        "hover":              hex_to_rgb(colorscheme["background_alt"]),
+    }
+    gen_plasma_colors(colors)
 
-output = template.format_map(colors)
-
-with open(f"{os.environ["HOME"]}/.local/share/color-schemes/{colorscheme_name}.colors", "w") as f:
-    f.write(output)
+if __name__ == '__main__':
+    main()
--- rofi/.config/rofi/scripts/change_theme.sh
+++ rofi/.config/rofi/scripts/change_theme.sh
@@ -1,69 +1,5 @@
-dotfiles_location="$HOME/dotfiles";
 themes_location="$HOME/dotfiles/themes";
 options=($(ls $themes_location));
-
-
-change_theme(){
-    if [ -z "$1" ];then
-        echo "no theme chosen";
-        exit 1;
-    fi
-
-    clear;
-    echo "Installing theme: $1";
-    local theme_location="$themes_location/$1";
-    echo "updating rofi...";
-    ln -fs $theme_location/rofi/colors.rasi $dotfiles_location/rofi/.config/rofi/colors.rasi;
-
-    echo "updating hypr...";
-    ln -fs $theme_location/hypr/colors.conf $dotfiles_location/hypr/.config/hypr/hyprland/colors.conf;
-
-    echo "updating kitty...";
-    ln -fs $theme_location/kitty/colors.conf $dotfiles_location/kitty/.config/kitty/colors.conf;
-
-    echo "updating alacritty...";
-    ln -fs $theme_location/alacritty/theme.toml $dotfiles_location/alacritty/.config/alacritty/theme.toml;
-
-    echo "updating nvim...";
-    ln -fs $theme_location/nvim/theme.lua $dotfiles_location/nvim/.config/nvim/lua/plugins/theme.lua;
-#    ln -fs $theme_location/nvim/vim-theme-options.lua $dotfiles_location/nvim/.config/nvim/lua/vim-theme-options.lua;
-
-    echo "updating swaync...";
-    ln -fs $theme_location/swaync/colors.css $dotfiles_location/swaync/.config/swaync/colors.css;
-
-    echo "updating tmux...";
-    ln -fs $theme_location/tmux/tmux-colors.conf $dotfiles_location/tmux/.config/tmux/tmux-colors.conf;
-
-    echo "updating wallpaper...";
-    actual_file=$(ls "$dotfiles_location/wallpaper/$1/background_$1"*); 
-    ln -fs "$actual_file" ~/.config/background;
-
-    echo "updating waybar...";
-    ln -fs $theme_location/waybar/colors.css $dotfiles_location/waybar/.config/waybar/colors.css;
-
-    echo "updating plasma...";
-    if [[ ! -e "$HOME/.local/share/color-schemes/$1.colors" ]];then
-         ~/dotfiles/plasma/plasma_colors.py $1
-    fi
-
-    echo "reload services";
-
-    echo "reload hyprland...";
-    hyprctl reload;
-
-    echo "relaod waybar...";
-    pkill waybar;
-    hyprctl dispatch exec waybar;
-
-    echo "reload hyprpaper...";
-    pkill hyprpaper; hyprctl dispatch exec "hyprpaper";
-
-    echo "reload swaync...";
-    pkill swaync; hyprctl dispatch exec swaync;
-
-    echo "reload plasma...";
-    plasma-apply-colorscheme $1
-}
 
 rofi_cmd() {
 	rofi -theme ~/.config/rofi/generic_list/theme.rasi \
@@ -75,4 +11,4 @@ rofi_cmd() {
 choice=$(printf '%s\n' "${options[@]}"|rofi_cmd );
 echo $choice
 
-change_theme $choice
+~/dotfiles/scripts/select_theme.sh $choice
