e43e4496b1e8b2f11f3f953e4162ca1c104121c1
aappio <alle.appio@gmail.com>
2026-04-28 23:44:13 +0200

    fix copilot verbosity
    Signed-off-by: aappio <alle.appio@gmail.com>

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -10,7 +10,6 @@ vim.g.copilot_tab_fallback = ""
 vim.g.copilot_enabled = false
 
 require("CopilotChat").setup({
-    debug = true,
     show_help = true,
     window = {
             layout = "vertical",
