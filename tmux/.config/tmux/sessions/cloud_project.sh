e939f4949532a6cc89179a32a383fc72e4241ada
aappio <alle.appio@gmail.com>
2026-06-30 12:27:17 +0200

    tmux edit sessions
    Signed-off-by: aappio <alle.appio@gmail.com>

---
+++ tmux/.config/tmux/sessions/cloud_project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SESSION_NAME="cloud_project"
+cd ~/DATA/universita/magistrale/anno2/semestre1/cloud
+tmux new-session -d -s $SESSION_NAME
+tmux send-keys -t $SESSION_NAME:1 "nvim" C-m
+tmux new-window -t $SESSION_NAME:2 -n 'test'
+tmux new-window -t $SESSION_NAME:3 -n 'generic'
+tmux split-window -t $SESSION_NAME:2 -h
+tmux switch-client -t $SESSION_NAME:1
--- tmux/.config/tmux/start_session.sh
+++ tmux/.config/tmux/start_session.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-choice=$(find ~/.config/tmux/sessions/* | fzf --style=full --color=base16 --layout=reverse --border=sharp)
+SESSIONS_PATH=~/.config/tmux/sessions 
+fzf_command() {
+    fzf --style=full --color='fg:white,pointer:blue,current-fg:white' --layout=reverse --border=sharp
+}
+
+choice=$(ls --color=never $SESSIONS_PATH | fzf_command)
 
 if [ -z "$choice" ]; then
     exit 0
 fi
 
-bash "$choice"
+bash "$SESSIONS_PATH/$choice"
