179b1b1a6c119826ac40ca4f7cdde8061bfaccfe
aappio <alle.appio@gmail.com>
2026-09-18 09:06:17 +0200

    Fri Sep 18 09:06:17 AM CEST 2026
    Signed-off-by: aappio <alle.appio@gmail.com>

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -91,29 +91,6 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 --     },
 -- })
 
--- Disable arrows to get better
-local hardmode = false
-if hardmode then
-    -- Show an error message if a disabled key is pressed
-    local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
-
-    -- Disable arrow keys in insert mode with a styled message
-    vim.api.nvim_set_keymap('i', '<Up>', '<C-o>' .. msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap('i', '<Down>', '<C-o>' .. msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap('i', '<Left>', '<C-o>' .. msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap('i', '<Right>', '<C-o>' .. msg, { noremap = true, silent = false })
-    -- vim.api.nvim_set_keymap('i', '<Del>', '<C-o>' .. msg, { noremap = true, silent = false })
-    -- vim.api.nvim_set_keymap('i', '<BS>', '<C-o>' .. msg, { noremap = true, silent = false })
-
-    -- Disable arrow keys in normal mode with a styled message
-    vim.api.nvim_set_keymap('n', '<Up>', msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap('n', '<Down>', msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap('n', '<Left>', msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap('n', '<Right>', msg, { noremap = true, silent = false })
-    -- vim.api.nvim_set_keymap('n', '<BS>', msg, { noremap = true, silent = false })
-end
-
-
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
--- rofi/.config/rofi/scripts/change_theme.sh
+++ rofi/.config/rofi/scripts/change_theme.sh
@@ -1,7 +1,9 @@
-themes_location="$HOME/dotfiles/theme_gen/outputs";
+themes_location="$HOME/dotfiles/theme_gen/themes";
 options=($(ls $themes_location));
 
-choice=$(printf '%s\n' "${options[@]}"|rofi -dmenu -p "> " -i);
+# choice=$(printf '%s\n' "${options[@]}"|rofi -dmenu -p "> " -i);
+choice=$(printf '%s\n' "${options[@]%.toml}" | rofi -dmenu -p "> " -i)
+[[ -z "$choice" ]] && exit 0
 echo $choice
 
 ~/dotfiles/theme_gen/theme_generator.py $choice
